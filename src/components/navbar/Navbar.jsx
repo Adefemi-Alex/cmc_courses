186d663dd8a184d16a43c5f608b2a3ade55432f1
Waptech <abdulfataiyakub07@gmail.com>
2024-04-02 17:09:28 +0100

    added z-index to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -81,7 +81,7 @@ function Navbar() {
         // className="flex items-center justify-between px-[3.5rem] py-5 w-full tablet:px-8 "
         // style={{ backgroundImage: `url(${back})` }}
 
-        className={`bg-white flex items-center justify-between px-[3.5rem] py-5 w-full fixed top-0 transition-all ${
+        className={`bg-white flex items-center justify-between px-[3.5rem] py-5 w-full fixed top-0 transition-all z-40 ${
           isScrolled ? "border-b-2 border-gray-200" : ""
         }`}
       >
