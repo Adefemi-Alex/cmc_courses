c6c654e786866efec66f2d930dbf02249c7cc5d8
Waptech <abdulfataiyakub07@gmail.com>
2024-04-02 18:24:04 +0100

    About Us page

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const About = () => {
+    return (
+        <>
+            <section className='py-10 bg-[radial-gradient(169.40%_89.55%_at_94.76%_6.29%,rgba(200,251,231,0.40)_10%,rgba(17,241,241,0.40)_40%,rgba(51,255,168,0.30)_90%)]'>
+                <div className=' w-full items-center justify-center text-center'>
+                    <h1 className="text-lg">About Us</h1>
+                    <span className='text-span text-fontSize1 sm:text-fontSize3'>Everything You Need To Know About ExpertSkillz</span>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default About
