000a41567ea8810b7070de75e50f3ef7f01d3e76
Waptech <abdulfataiyakub07@gmail.com>
2024-04-03 15:27:02 +0100

    updated testimonials

--- src/components/about/Testimonials.jsx
+++ src/components/about/Testimonials.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import about from "../../assets/images/about/about.svg";
+import '../../pages/about/About.css'
+
+import about from "../../assets/images/about/decorative_images/decorativeImage.svg";
+import about1 from "../../assets/images/about/decorative_images/decorativeImage2.svg";
+import about2 from "../../assets/images/about/decorative_images/decorativeImage3.svg";
 
 const Testimonials = () => {
     return (
@@ -9,22 +13,29 @@ const Testimonials = () => {
                     <h1 className="text-fontSize1 sm:text-fontSize2">Testimonials</h1>
                     <span className='text-fontSize3 sm:text-fontSize4 sm:mx-1'>Join Thousands of Satisfied Learners Who Have Achieved Success With ExpertSkillz. Hear What Our Users Have To Say About Us:</span>
                 </div>
-                <div className='w-full items-center justify-center'>
-                    <div className='relative sm:my-10 mx-40'>
-                        <img src={about} alt="Card Image" className="absolute -top-11 -right-1 -z-50 sm:w-50 sm:-right-8" />
-                        <div className="bg-white justify-center items-center text-center mx-20 px-20">
-                            <h2>Adejumo Eniola</h2>
-                            <span className='text-span text-[200px]'>“</span>
-                            <p className='text-fontSize3 text-base relative'>
-                                ExpertSkillz has been a total game-changer for me in the online world!
-                                Their first course which is their flagship course on Affiliate Marketing “CMC” is top-notch.
-                                With the trainings I got on CMC, I have seen massive results in no time even as a newbie!
-                                Another thing is, the mentorship on CMC is super beginner-friendly. <br></br><br></br>
-                            </p>
-                            <span className='text-span text-[200px]'>“</span>
+                <div className='items-center justify-center relative py-10'>
+                    <img src={about2} alt="Card Image" className="absolute top-20 left-32 -z-50 sm:w-50 sm:top-0 sm:left-16" />
+                    <img src={about1} alt="Card Image" className="absolute top-10 left-20 -z-50 sm:w-50 sm:-top-5 sm:left-5" />
+                    <div className='relative sm:my-10 mx-60 sm:mx-2 my-10'>
+                        <img src={about} alt="Card Image" className="absolute -top-11 right-0 -z-50 sm:w-50 sm:-right-0" />
+                        <div className="bg-white justify-center items-center text-center mx-20 px-20 sm:m-5 sm:p-5 relative">
+                            <h2 className='pt-10 text-fontSize2'>Adejumo Eniola</h2>
+                            <div className='flex py-10'>
+                                <span className='text-span text-[200px] absolute left-10 top-0'>“</span>
+                                <p className='text-fontSize3 text-base px-10 w-full'>
+                                    ExpertSkillz has been a total game-changer for me in the online world!
+                                    Their first course which is their flagship course on Affiliate Marketing “CMC” is top-notch.
+                                    With the trainings I got on CMC, I have seen massive results in no time even as a newbie!
+                                    Another thing is, the mentorship on CMC is super beginner-friendly. <br></br><br></br>
+                                </p>
+                                <span className='text-span text-[200px] absolute right-10 -bottom-7 rotate-180'>“</span>
+                            </div>
+                            <div className="triangle rotate-180 w-10 h-20"></div>
                         </div>
-                        <img src={about} alt="Card Image" className="absolute -bottom-10 -left-8 -z-50 sm:w-50" />
+                        <img src={about} alt="Card Image" className="absolute -bottom-10 left-0 -z-50 sm:w-50" />
                     </div>
+                    <img src={about1} alt="Card Image" className="absolute bottom-20 right-32 -z-50 sm:w-50 sm:bottom-0 sm:right-16" />
+                    <img src={about2} alt="Card Image" className="absolute bottom-10 right-20 -z-50 sm:w-50 sm:-bottom-5 sm:right-5" />
                 </div>
             </section>
         </>
