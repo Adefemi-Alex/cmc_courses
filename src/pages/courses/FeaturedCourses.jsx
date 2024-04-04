0f70cd13c6dad2cc9665095a842a92d4aaf17cd7
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-04-05 00:56:55 +0100

    created a new file featured courses

---
+++ src/pages/courses/FeaturedCourses.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FeaturedCourses = () => {
+  return (
+    <>
+      <div className="py-20">
+        <h1 className="text-black text-center capitalize text-fontSize1 sm:text-fontSize3">
+          featured courses
+        </h1>
+
+        <section className="Recommended py-16">
+          <h1 className="text-black capitalize text-fontSize2 sm:text-fontSize3">
+            recommended courses
+          </h1>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default FeaturedCourses;
