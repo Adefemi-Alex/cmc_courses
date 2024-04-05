7e932479094510b76db41a79cdaca24fd7daa27f
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-04-05 09:11:06 +0100

    created the affliliate section

---
+++ src/pages/courses/CoursesAffiliate.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CoursesAffiliate = () => {
+  return (
+    <>
+      <section className="affiliate py-14 px-10">
+        <h1 className="text-black text-center capitalize text-fontSize1 sm:text-fontSize3">
+          affliated courses
+        </h1>
+
+        <div className="affiliate-container pt-8"></div>
+      </section>
+    </>
+  );
+};
+
+export default CoursesAffiliate;
