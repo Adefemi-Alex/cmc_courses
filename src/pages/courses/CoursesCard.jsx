8118b904af3cede22ef4360346c11008e7db4448
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-04-05 05:13:16 +0100

    made changes

--- src/pages/courses/CoursesCard.jsx
+++ src/pages/courses/CoursesCard.jsx
@@ -1,22 +1,25 @@
-import courseimg1 from "../../assets/images/rec1.jfif";
-import courseimg2 from "../../assets/images/rec2.jfif";
-import courseimg3 from "../../assets/images/rec3.jfif";
+import rec1 from "../../assets/images/rec1.jfif";
+import rec2 from "../../assets/images/rec2.jfif";
+import rec3 from "../../assets/images/rec3.jfif";
+import courseimg1 from "../../assets/images/courseimg1.png";
+import courseimg2 from "../../assets/images/courseimg2a.jfif";
+import courseimg3 from "../../assets/images/courseimg3a.jfif";
 
 export const recommended_Courses_Data = [
   {
-    image: courseimg1,
+    image: rec1,
     title: "UI and Interactive Design",
     description:
       "Discover lucrative design opportunities. Explore fields such as Graphic design, Product design, UI/UX Design and more. In this class, you’ll learn how to master design techniques and monetize your skills by offering professional services to companies and business owners.",
   },
   {
-    image: courseimg2,
+    image: rec2,
     title: "Online Investment",
     description:
       "Discover our range of finance skills, including various investment skills like Crypto Trading, Forex Trading, Crypto Arbitrage, and more. In this class you’ll learn how to generate substantial income through these investment skills, all from the comfort of your home.",
   },
   {
-    image: courseimg3,
+    image: rec3,
     title: "Designing Skills",
     description:
       "Discover lucrative design opportunities. Explore fields such as Graphic design, Product design, UI/UX Design and more. In this class, you’ll learn how to master design techniques and monetize your skills by offering professional services to companies and business owners.",
