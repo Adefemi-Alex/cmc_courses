91f2a07a57219fc9f917666c0719e130fe9dc3e1
RealRiga <116930426+RealRiga@users.noreply.github.com>
2024-04-06 23:41:35 +0100

    did changes on the home page and re-arranged it also added the affliate to the navbar and created a folder and file for it

---
+++ src/pages/home/HowItWorks.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import ChooseLine from "../../assets/images/ChooseLine.png";
+import vector from "../../assets/images/Vector3.png";
+import background from "../../assets/images/background.png";
+
+const HowItWorks = () => {
+  return (
+    <>
+      {/* How it works */}
+
+      <div className="-mt-20 z-0 w-full">
+        {/* Arrow and the spiral background image section */}
+        <div className="flex justify-between mb-2">
+          <div className="ml-30 mt-40  w-full h-full">
+            <img
+              src={ChooseLine}
+              alt="line"
+              className="ml-[57%] mt-[6px] w-[200px] h-full object-cover"
+            />
+          </div>
+          {/* Centered image */}
+          <div className="w-[650px]">
+            <img src={background} alt="line" className="w-full object-cover" />
+          </div>
+        </div>
+        {/* Header section */}
+        <div className="flex justify-center items-center -mt-60">
+          <h1 className="ml-8 text-4xl lg:text-5xl xl:text-6xl relative">
+            Here's How It{" "}
+            <span className="relative inline-block w-[170px]">
+              Works
+              <img
+                src={vector}
+                alt="line"
+                className="absolute top-1/3 transform -translate-y-1/2 right-3 z-0 h-[70px] w-[400px]"
+              />
+            </span>
+          </h1>
+        </div>
+
+        <h2 className="text-2xl mt-7 flex justify-center items-center">
+          With ExpertSkillz, It's Easy To Learnand Earn
+        </h2>
+        <p className="mt-7 flex justify-center items-center mx-60">
+          Stop struggling alone,Learning and succeding online hasn't been easy
+          for many. With ExpertSkillz, it's easier than you think.
+        </p>
+        {/* Values */}
+        <section>
+          <div className="grid grid-cols-3 md:grid-cols-2 gap-28 sm:gap-10 justify-between my-20 sm:m-10 mx-28">
+            <div className="bg-white rounded-lg p-5 drop-shadow-[0_10px_10px_rgba(51,255,168,1)]">
+              <div className="rounded-lg bg-white drop-shadow-[0_10px_10px_rgba(13,12,56,0.15)] p-2 mb-5 w-12 text-center">
+                <span className="text-span text-lg">1</span>
+              </div>
+              <p className="my-3">Choose a Course</p>
+              <p className="text-gray-500 text-sm">
+                Choose from our variety of courses to start learning.{" "}
+              </p>
+            </div>
+            <div className="bg-white rounded-lg p-5 drop-shadow-[0_10px_10px_rgba(51,255,168,1)]">
+              <div className="rounded-lg bg-white drop-shadow-[0_10px_10px_rgba(13,12,56,0.15)] p-2 mb-5 w-12 text-center">
+                <span className="text-span text-lg">2</span>
+              </div>
+              <p className="my-3">Get Started and Pay</p>
+              <p className="text-gray-500 text-sm">
+                Get started effortlessly and complete your payment securely.{" "}
+              </p>
+            </div>
+            <div className="bg-white rounded-lg p-5 drop-shadow-[0_10px_10px_rgba(51,255,168,1)]">
+              <div className="rounded-lg bg-white drop-shadow-[0_10px_10px_rgba(13,12,56,0.15)] p-2 mb-5 w-12 text-center">
+                <span className="text-span text-lg">3</span>
+              </div>
+              <p className="my-3">Learn and Earn</p>
+              <p className="text-gray-500 text-sm">
+                Learn valuable skills and start earning from your expertise.{" "}
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
---
+++ src/pages/home/StudentsRemark.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ChooseLine from "../../assets/images/ChooseLine.png";
+import vector from "../../assets/images/Vector3.png";
+import background from "../../assets/images/background.png";
+import Testimonials from "./Testimonial";
+
+const StudentsRemark = () => {
+  return (
+    <>
+      {/* Students Remark */}
+
+      <div className="-mt-40 z-0 w-full">
+        {/* Arrow and the spiral background image section */}
+        <div className="flex justify-between mb-2">
+          <div className="ml-30 mt-40  w-full h-full">
+            <img
+              src={ChooseLine}
+              alt="line"
+              className="ml-[57%] mt-[6px] w-[200px] h-full object-cover"
+            />
+          </div>
+          {/* Centered image */}
+          <div className="w-[650px]">
+            <img src={background} alt="line" className="w-full object-cover" />
+          </div>
+        </div>
+        {/* Header section */}
+        <div className="flex justify-center items-center -mt-[190px]">
+          <h1 className="ml-8 text-4xl lg:text-5xl xl:text-6xl relative">
+            What{" "}
+            <span className="relative inline-block ml-3 -mr-3 w-[180px]">
+              Students
+              <img
+                src={vector}
+                alt="line"
+                className="absolute top-1/3 transform -translate-y-1/2 right-4 z-0 h-[80px] w-[190px]"
+              />
+            </span>
+            Say About Us
+          </h1>
+        </div>
+        <Testimonials />
+      </div>
+    </>
+  );
+};
+
+export default StudentsRemark;
