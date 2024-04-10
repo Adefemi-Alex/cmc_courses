e9aeb1abc126170ddb70840401f09c27c6798965
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-04-10 22:58:44 +0100

    Done with the faq section

--- src/pages/courses/CoursesFaq.jsx
+++ src/pages/courses/CoursesFaq.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { data } from "../../faqdata";
 import { IoIosArrowForward } from "react-icons/io";
 const CoursesFaq = () => {
   const [openStates, setOpenStates] = useState(Array(data.length).fill(false));
-
+  const [moreInfo, setMoreInfo] = useState(Array(data.length).fill(false));
   const toggleOpen = (index) => {
     const newOpenStates = [...openStates];
     newOpenStates[index] = !newOpenStates[index];
     setOpenStates(newOpenStates);
+  };
+  const toggleInfo = (index) => {
+    moreInfo[index] = !moreInfo[index];
+    setMoreInfo([...moreInfo]);
   };
   return (
     <>
@@ -21,12 +26,12 @@ const CoursesFaq = () => {
         flex-col space-y-8"
         >
           {data.slice(0, 4).map(({ id, question, answer }, index) => (
-            <div
-              className="faq-each bg-white text-black w-[896px] max-w-[80%] h-auto p-11 rounded-2xl"
-              key={id}
-              onClick={() => toggleOpen(index)}
-            >
-              <div className="question-section flex justify-between items-center">
+            <div className="faq-each bg-white text-black w-[896px] max-w-[80%] h-auto p-11 rounded-2xl">
+              <div
+                className="question-section flex justify-between items-center"
+                key={id}
+                onClick={() => toggleOpen(index)}
+              >
                 <h2>{question}</h2>
                 <div
                   className={`arrow p-2 background3 text-white rounded-full transition-all duration-500 ease-in-out   ${
@@ -36,19 +41,36 @@ const CoursesFaq = () => {
                   <IoIosArrowForward size={25} className="cursor-pointer" />
                 </div>
               </div>
-              <p
-                className={`mt-4 text-[#6F6C90] line-clamp-2  transition-all duration-500 ease-in-out -translate-y-12
-                ${
-                  openStates[index]
-                    ? "translate-y-0 opacity-1 h-full"
-                    : "h-0 opacity-0"
+              <div
+                className={`mt-4 transition-all duration-500 ease-in-out  ${
+                  openStates[index] ? "opacity-1 h-full" : "h-0 opacity-0"
                 }`}
               >
-                {answer}
-              </p>
-              <span className="cursor-pointer">See more</span>
+                <p
+                  className={` text-[#6F6C90] ${
+                    moreInfo[index] ? "line-clamp-none" : "line-clamp-2"
+                  }  
+               `}
+                >
+                  {answer}
+                </p>
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    toggleInfo(index);
+                  }}
+                >
+                  {moreInfo[index] ? "See less" : "See more"}
+                </span>
+              </div>
             </div>
           ))}
+
+          <Link to="/faqs">
+            <p className="text-black font-bold text-lg background4 rounded-3xl border-[1px] border-solid border-black px-8 py-2">
+              See more
+            </p>
+          </Link>
         </div>
       </div>
     </>
