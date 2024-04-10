09fcce0d86a3f6c9c333a2206934b2f1aff620d5
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-04-10 02:28:25 +0100

    For faq...remains the slide imlementaion

--- src/pages/courses/CoursesFaq.jsx
+++ src/pages/courses/CoursesFaq.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { data } from "../../faqdata";
+import { IoIosArrowForward } from "react-icons/io";
 const CoursesFaq = () => {
   return (
     <>
@@ -8,7 +9,37 @@ const CoursesFaq = () => {
           Frequently Asked Questions (FAQs):
         </h1>
 
-        <div className="faq-inner  bg-[#F7F7FB] mt-24 w-full h-[600px] p-6"></div>
+        <div
+          className="faq-inner  bg-[#F7F7FB] mt-24 py-16 px-8 flex justify-center items-center 
+        flex-col space-y-8"
+        >
+          {data.slice(0, 4).map(({ id, question, answer, open }) => {
+            return (
+              <div
+                className="faq-each bg-white text-black w-[896px] 
+                max-w-[80%] h-auto p-11 rounded-2xl"
+                key={id}
+                onClick={() => {
+                  open = !open;
+                  console.log(open);
+                }}
+              >
+                <div className="question-section flex justify-between ">
+                  <h2>{question}</h2>
+                  <div className="arrow p-2 background3 text-white rounded-full">
+                    <IoIosArrowForward size={25} className="cursor-pointer" />
+                  </div>
+                </div>
+                <p
+                  className={`mt-4 text-[#6F6C90] line-clamp-2 h-0
+                  ${open ? "h-full" : "h-full"}`}
+                >
+                  {answer}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
