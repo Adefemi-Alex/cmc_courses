4034d7d1af4228b1ea89a02e4f95b9ae5933e6fc
Waptech <abdulfataiyakub07@gmail.com>
2024-04-13 13:13:28 +0100

    created affliate page

--- src/pages/Affliate/Affliate.jsx
+++ src/pages/Affliate/Affliate.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Affliate1 from "../../components/affliate/Affliate1";
 
 const Affliate = () => {
   return (
-    <div>Affliate</div>
-  )
-}
+    <>
+      <section className="py-10 gradient-background">
+        <div className=" w-full items-center justify-center text-center">
+          <h1 className="text-lg">AFFLIATE</h1>
+        </div>
+        <Affliate1/>
+      </section>
+    </>
+  );
+};
 
-export default Affliate
+export default Affliate;
