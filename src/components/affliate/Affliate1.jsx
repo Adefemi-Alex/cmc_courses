dc0ba69681421bbf6744a478e196ac3efe3e2a1f
Waptech <abdulfataiyakub07@gmail.com>
2024-04-13 13:14:42 +0100

    first section of the affliate page

---
+++ src/components/affliate/Affliate1.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import affliateimg from "../../assets/images/affliate/affliate.png";
+import decor from "../../assets/images/decorative_images/decorativeImage.svg";
+
+const Affliate1 = () => {
+  return (
+    <>
+      <section>
+        <div className="flex sm:flex-col items-center justify-between px-20 py-20 sm:p-3">
+          <div className="flex-1 mx-10 sm:m-3">
+            <h2 className="text-fontSize2 sm:text-fontSize4 text-black font-bold mb-5">
+              Join Our Affiliate Program
+            </h2>
+            <p className="text-gray-500 text-base sm:text-sm">
+              Work from home with your smartphone as an affiliate for our
+              platform and receive weekly payments. Whether you’re a beginner
+              learning from our platform or an experienced affiliate, we ensure
+              to provide the best opportunities for working and earning. Don’t
+              waste time; register as an affiliate now.
+            </p>
+            <div className="flex items-center justify-center">
+              <p className="text-black  background3 rounded-full border border-solid border-black px-8 py-3 font-bold text-center">
+                Become an Affliate
+              </p>
+            </div>
+          </div>
+          <div className="relative flex-1 mx-10 sm:my-10">
+            <img
+              src={decor}
+              alt="Card Image"
+              className="absolute -top-11 -right-1 -z-50 sm:w-50 sm:-right-8"
+            />
+            <img
+              src={affliateimg}
+              alt="Card Image"
+              className="z-50 mx-10 sm:m-0"
+            />
+            <img
+              src={decor}
+              alt="Card Image"
+              className="absolute -bottom-10 -left-8 -z-50 sm:w-50"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Affliate1;
