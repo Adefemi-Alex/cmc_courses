d0b8a398467ca4e834269284a15b3e3f086ca528
Waptech <abdulfataiyakub07@gmail.com>
2024-04-13 14:01:36 +0100

    update

--- src/components/affliate/AffliateButton.jsx
+++ src/components/affliate/AffliateButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AffliateButton = () => {
   return (
     <>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center my-2">
         <p className="text-black  background3 rounded-full border border-solid border-black px-8 py-3 font-bold text-center">
           Become an Affliate
         </p>
