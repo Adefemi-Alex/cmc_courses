6298a246270b1e0a8d60f00a6fb0ae1b99b6f86b
Waptech <abdulfataiyakub07@gmail.com>
2024-04-13 13:56:48 +0100

    created become an affliate button component

---
+++ src/components/affliate/AffliateButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AffliateButton = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <p className="text-black  background3 rounded-full border border-solid border-black px-8 py-3 font-bold text-center">
+          Become an Affliate
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AffliateButton;
