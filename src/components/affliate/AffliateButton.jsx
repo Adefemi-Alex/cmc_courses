efbbceb7cbc2b64e6d00736752449e1a766dd672
Waptech <abdulfataiyakub07@gmail.com>
2024-04-26 11:28:05 +0100

    added props to affiliate button

--- src/components/affliate/AffliateButton.jsx
+++ src/components/affliate/AffliateButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const AffliateButton = () => {
+const AffliateButton = ({text}) => {
   return (
     <>
       <div className="flex items-center justify-center my-2">
         <p className="text-black  background3 rounded-full border border-solid border-black px-8 py-3 font-bold text-center">
-          Become an Affliate
+          {text}
         </p>
       </div>
     </>
