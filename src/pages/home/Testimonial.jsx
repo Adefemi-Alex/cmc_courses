33a083d2118ced6c2e346b9fc8eabcac81c73458
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-04-27 18:57:25 +0100

    fixed slider in other pages and deleted unnecessary d file

--- src/pages/home/Testimonial.jsx
+++ src/pages/home/Testimonial.jsx
@@ -7,16 +7,16 @@ import "../../pages/about/About.css";
 import person1 from "../../assets/images/Testimonial1.png";
 import person2 from "../../assets/images/Testimonial2.png";
 import person3 from "../../assets/images/Testimonial3.png";
-import about from "../../assets/images/decorative_images/decorativeImage.svg";
-import about1 from "../../assets/images/decorative_images/decorativeImage2.svg";
-import about2 from "../../assets/images/decorative_images/decorativeImage3.svg";
+import decor1 from "../../assets/images/decorative_images/Ornament 63.svg";
+import decor2 from "../../assets/images/decorative_images/Ornament 64.png";
+import decor3 from "../../assets/images/decorative_images/Ornament 65.svg";
 import prevImage from "../../assets/images/leftArrow.png";
 import nextImage from "../../assets/images/rightArrow.png";
 
 const Testimonials = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const testimonialsData = [
+  const topAffliatesData = [
     {
       name: "Adejumo Eniola",
       quote:
@@ -41,11 +41,23 @@ const Testimonials = () => {
     dots: true,
     infinite: true,
     speed: 500,
+    className: "center",
+    centerMode: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
-    beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex),
+    autoplaySpeed: 2000,
+    appendDots: (dots) => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    ),
+    afterChange: (oldIndex, newIndex) => setCurrentSlide(newIndex),
   };
 
   const sliderRef = React.useRef(null);
@@ -65,20 +77,30 @@ const Testimonials = () => {
   };
 
   return (
-    <section>
+    <section className="overflow-hidden">
+      <div className="w-full items-center justify-center text-center mt-20 mb-12">
+        <h1 className="text-fontSize1 sm:text-fontSize2">
+          Hear From Our Top Affliates
+        </h1>
+      </div>
       <div className="items-center justify-center relative py-10">
         <img
-          src={about2}
+          src={decor1}
           alt="Card Image"
-          className="absolute top-20 left-32 -z-50 sm:w-50 sm:top-0 sm:left-16"
+          className="absolute -top-8 right-52 -z-50 sm:w-50 sm:-right-0"
         />
         <img
-          src={about1}
+          src={decor2}
           alt="Card Image"
-          className="absolute top-10 left-20 -z-50 sm:w-50 sm:-top-5 sm:left-5"
+          className="absolute top-32 right-36 -z-50 sm:w-50 sm:-right-0"
+        />
+        <img
+          src={decor3}
+          alt="Card Image"
+          className="absolute top-4 right-28 -z-50 sm:w-50 sm:-right-0"
         />
         <Slider {...settings} initialSlide={currentSlide} ref={sliderRef}>
-          {testimonialsData.map((affliate, index) => (
+          {topAffliatesData.map((affliate, index) => (
             <div key={index}>
               <div className="relative sm:my-10 mx-20 sm:mx-2 my-10">
                 <div className="bg-white flex gap-3 justify-center items-center text-center mx-20 px-20 sm:m-5 sm:p-5 relative rounded-3xl">
@@ -108,13 +130,11 @@ const Testimonials = () => {
           ))}
         </Slider>
 
-        <div className="flex justify-center">
-          <div className="absolute  -translate-y-1/2 left-[500px] cursor-pointer w-16">
-            <img src={prevImage} alt="Prev" onClick={goToPrevSlide} />
-          </div>
-          <div className="absolute -translate-y-1/2 right-[500px] cursor-pointer w-16">
-            <img src={nextImage} alt="Next" onClick={goToNextSlide} />
-          </div>
+        <div className="absolute top-1/3 -translate-y-1/2 left-[150px] cursor-pointer w-16">
+          <img src={prevImage} alt="Prev" onClick={goToPrevSlide} />
+        </div>
+        <div className="absolute top-1/3 -translate-y-1/2 right-[150px] cursor-pointer w-16">
+          <img src={nextImage} alt="Next" onClick={goToNextSlide} />
         </div>
       </div>
     </section>
