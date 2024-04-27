6e89d506710a9580a1d475c29d233e7aebccd019
RealRiga <116930426+RealRiga@users.noreply.github.com>
2024-04-27 10:19:38 +0100

    changes on the button page

--- src/components/affliate/AffliateButton.jsx
+++ src/components/affliate/AffliateButton.jsx
@@ -5,10 +5,6 @@ const AffliateButton = () => {
   return (
     <>
       <div className="flex items-center justify-center my-2">
-        {/* <p className="text-black  background3 rounded-full border border-solid border-black px-8 py-3 font-bold text-center">
-          Become an Affliate
-        </p> */}
-
         <NavLink to="/affliatelogin">
           <button className="background3 hover:background text-black font-bold py-2 px-4 rounded-full">
             Become an Affliate
